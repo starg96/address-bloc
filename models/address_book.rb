a839af8c96046c60bbfaeb722175c75e173e2e76
Kyle <starg96@gmail.com>
2015-08-29 19:40:57 -0500

    completed assignment 21

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,16 @@
  		end
  		@entries.insert(index, Entry.new(name, phone, email))
  	end
+
+ 	def remove_entry(name, phone, email)
+ 		delete_entry = nil
+
+ 		@entries.each do |entry|
+ 			if name == entry.name && phone == entry.phone_number && email = entry.email
+ 				delete_entry = entry
+ 			end
+ 		end
+
+ 		@entries.delete(delete_entry)
+ 	end
  end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,20 @@ RSpec.describe AddressBook do
 		end
 	end
 
+	context "#remove_entry" do
+		it "removes entry from the address book" do
+			book = AddressBook.new
+			book.add_entry('Homer Simpson', '010.032.2825', 'homer.simpson@simpson.com')
+
+			name = "Ada Lovelace"
+			phone_number = "010.012.1815"
+			email_address = "augusta.king@lovelace.com"
+			book.add_entry(name, phone_number, email_address)
+
+			expect(book.entries.size).to eq 2
+			book.remove_entry(name, phone_number, email_address)
+			expect(book.entries.size).to eq 1
+			expect(book.entries.first.name).to eq("Homer Simpson")
+		end
+	end
 end
