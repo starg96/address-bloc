fa038d2527fd4c3de3fa80e1b2dcecf8278eca69
Kyle <starg96@gmail.com>
2015-09-01 21:38:41 -0500

    finished assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -60,4 +60,13 @@
  		end
  		return nil
  	end
+
+ 	def iterative_search(name)
+ 		@entries.each do |entry|
+ 			if entry.name == name
+ 				return entry
+ 			end
+ 		end
+ 		return nil
+ 	end
  end
